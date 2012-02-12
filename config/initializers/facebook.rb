0b50d823f5b7f62942b34ee868fd48c5a6821898
bzhang <bzhang@optaros.com>
2012-02-11 19:42:00 -0500

    corrected facebook key issue

--- config/initializers/facebook.rb
+++ config/initializers/facebook.rb
@@ -1,5 +1,6 @@
-facebook_settings = YAML.load_file(Rails.root.join('config', 'facebook.yml'))[Rails.env]
-FACEBOOK_APP_ID = facebook_settings['app_id']
-FACEBOOK_SECRET_KEY = facebook_settings['secret_key']
-
+if Rails.env.development?
+  facebook_settings = YAML.load_file(Rails.root.join('config', 'facebook.yml'))[Rails.env]
+  FACEBOOK_APP_ID = facebook_settings['app_id']
+  FACEBOOK_SECRET_KEY = facebook_settings['secret_key']
+end
 
